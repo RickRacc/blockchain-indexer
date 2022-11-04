0a5f24539e10e0172747337e763565235889f027
code-dev-ai <kevinx8888@gmail.com>
2022-11-03 21:42:02 -0500

    update test to use suite

--- repository/transaction_payment_repository_test.go
+++ repository/transaction_payment_repository_test.go
@@ -1,1 +1,56 @@
 package repository
+
+import (
+	"context"
+	"database/sql"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"go-bonotans/di"
+	"go-bonotans/model"
+	"math/big"
+	"os"
+	"testing"
+)
+
+type BlockTestSuite struct {
+	suite.Suite
+	repo *BlockRepository
+	pool *sql.DB
+}
+
+func (suite *BlockTestSuite) SetupAllSuite() {
+	var err error
+	suite.pool, err = di.ProvideDbPool()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	suite.repo = di.ProvideBlockRepository(pool)
+}
+
+func (suite *BlockTestSuite) TearDownAllSuite() {
+	err := suite.pool.Close()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func (suite *BlockTestSuite) TestCreateBlock() {
+	assert := assert.New(suite.T())
+	block := model.Block{
+		ParentHash:   "parenthash",
+		Hash:         "hash",
+		Number:       new(big.Int).SetInt64(1),
+		Transactions: nil,
+	}
+
+	b, err := repo.Process(context.Background(), &block)
+	assert.NoError(err, "Saving block returned an error")
+	assert.NotNil(b.Id)
+	assert.NotNil(b.CreatedAt)
+	assert.NotNil(b.UpdatedAt)
+}
+
+func TestBlockTestSuite(t *testing.T) {
+	suite.Run(t, new(BlockTestSuite))
+}
