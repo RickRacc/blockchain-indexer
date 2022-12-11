8ceeea1d9da7a476b99263f9b427083dd2c33e96
RickRacc <rakesh.p.singh2030@gmail.com>
2022-12-11 12:51:11 -0600

    clean up all data before running tests

--- test/base_test_suite.go
+++ test/base_test_suite.go
@@ -26,11 +26,31 @@ func (suite *BaseTestSuite) SetupSuite() {
 	if err != nil {
 		panic(err)
 	}
+	suite.deleteAllData()
 }
 
 func (suite *BaseTestSuite) TearDownSuite() {
 	err := suite.Pool.Close()
 	if err != nil {
 		os.Exit(1)
+	}
+}
+
+func (suite *BaseTestSuite) deleteAllData() {
+	_, err := suite.Pool.Exec("TRUNCATE indexer_position CASCADE")
+	if err != nil {
+		panic(err)
+	}
+	_, err = suite.Pool.Exec("TRUNCATE block CASCADE")
+	if err != nil {
+		panic(err)
+	}
+	_, err = suite.Pool.Exec("TRUNCATE eth_transaction CASCADE")
+	if err != nil {
+		panic(err)
+	}
+	_, err = suite.Pool.Exec("TRUNCATE transaction_payment CASCADE")
+	if err != nil {
+		panic(err)
 	}
 }
